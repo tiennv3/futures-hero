ff7ea2602d58fddf2bb17fca45b940b4a143e83d
laoowai <75831219+laoowai@users.noreply.github.com>
2022-08-31 15:15:52 +0800

    Update webhook_launcher.py

--- webhook_launcher.py
+++ webhook_launcher.py
@@ -3,8 +3,8 @@ import os, time, config
 
 def telegram_bot_sendtext(bot_message):
     
-    bot_token = '5004712342:AAEh0rTB5dWhlx_YITgDeDyX2awN8g3JLEM'
-    bot_chatID = '-667139609'
+    bot_token = ' '
+    bot_chatID = ' '
     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=html&text=' + bot_message
     response = requests.get(send_text)
     return response.json()
